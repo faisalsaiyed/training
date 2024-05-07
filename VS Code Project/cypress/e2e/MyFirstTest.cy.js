1071671397c17e72e33ad8bb23cd8cc1128c2568
faisalsaiyed <saiyed.faisal@gmail.com>
2024-05-07 16:54:38 -0500

    added test case

--- VS Code Project/cypress/e2e/MyFirstTest.cy.js
+++ VS Code Project/cypress/e2e/MyFirstTest.cy.js
@@ -2,4 +2,8 @@ describe('My First Test', () => {
     it('Does not do much!', () => {
       expect(true).to.equal(true)
     })
+
+    it('Does assert!', () => {
+      expect(true).to.equal(true)
+    })
   })
