9d838a6d3be1c0b43c5240bb5439568872ea2c9d
faisalsaiyed <saiyed.faisal@gmail.com>
2024-05-07 17:00:44 -0500

    removed test case

--- VS Code Project/cypress/e2e/MyFirstTest.cy.js
+++ VS Code Project/cypress/e2e/MyFirstTest.cy.js
@@ -2,8 +2,4 @@ describe('My First Test', () => {
     it('Does not do much!', () => {
       expect(true).to.equal(true)
     })
-
-    it('Does assert!', () => {
-      expect(true).to.equal(true)
-    })
   })
